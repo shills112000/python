a5908ecb5e4bbb26f71af82669f913a396aeb073
Simon Hills <simon.hills@gmail.com>
2020-03-04 11:46:20 +0000

    updated

---
+++ numbers.py
@@ -0,0 +1,23 @@
+# Intergers
+# floating point numbers
+
+# simple addition
+print (2 + 2)
+print (2 - 1)
+print (2 * 2)
+print (3 / 2)
+
+# Mods
+print (7 % 4)  # mod
+
+print ( 23 % 2)  # if you mod by 2 the output is 1 it's odd, if 0 it's even
+print ( 1001 % 2)
+print ( 20000000 % 2)
+
+#powers
+print ( 2 ** 3)  # 2 to the power of 3
+
+# order of operation
+
+print (2 + 10 * 10 + 3)
+print ((2+10) (10+3))
---
+++ varibles.py
@@ -0,0 +1,19 @@
+# variables cannot start with a numbers
+# There cannot be spaces , use _ instead
+# canoot use  :"",<>/>?|\()!@#$%^&*~-+
+# Names lowercase
+# varialbes can be assigned to different data types
+
+my_dogs = 2
+print (my_dogs)
+
+my_dogs = my_dogs + 8  # This will be 10
+print (my_dogs)
+
+# type  , tells you what data type a variable is
+
+type (my_dogs)
+
+my_dogs = "test"
+
+print (my_dogs)
