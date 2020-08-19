1e6ccb856242196cd05b9ee2d35e4a75438bed23
shills11200 <Simon.Hills@dcs.tech>
2020-08-19 10:16:58 +0000

    added

---
+++ METHODS_FUNCTIONS/documentation_on_methods.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3.7
+
+mylist= [1,2,3]
+
+mylist.append(4) # append
+
+mylist.pop() #pop off last item
+
+
+help(mylist.insert) # This gives the help on the method
+
+## USE python doc  docs.python.org
+
+
